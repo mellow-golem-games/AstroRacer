06525525dd97b5599ec93ef907fce01eaf387e3c
peterjewicz <peterjewicz@totalwebconnections.com>
2019-03-18 17:36:40 -0500

    add wall impulse

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,26 +6,63 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float speed = 6f; // Speed of the ship
+
+    private Rigidbody2D rb;
     private Vector2 target;
     private Vector2 currentPosition;
+    private bool isRebounding = false;
+    private int reboundTimer = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+      rb = GetComponent<Rigidbody2D>();
+    }
 
+    void OnCollisionEnter2D(Collision2D col) {
+      if (col.gameObject.tag == "wall") {
+        handleWallCollision();
+      }
+    }
+
+    private void handleWallCollision() {
+      // check the ship position
+      // if position.x < 0 force to right else force to left
+      // simulates the different sides of the walls.
+
+      if (gameObject.transform.position.x < 0) {
+        rb.AddForce(new Vector2(3,0), ForceMode2D.Impulse);
+      } else {
+        rb.AddForce(new Vector2(-3,0), ForceMode2D.Impulse);
+      }
+      isRebounding = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetMouseButton(0)) {
-  			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        currentPosition = gameObject.transform.position;
 
-        // manually override non moveable direction
-        target.y = currentPosition.y;
+      // rb.velocity = Vector2.zero;
 
-  			transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
-  		}
+      if(!isRebounding) {
+
+        if (Input.GetMouseButton(0)) {
+    			target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+          currentPosition = gameObject.transform.position;
+
+          // manually override non moveable direction
+          target.y = currentPosition.y;
+
+    			transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
+    		}
+      } else {
+        reboundTimer++;
+
+        if(reboundTimer >= 30) {
+          reboundTimer = 0;
+          isRebounding = false;
+          rb.velocity = Vector2.zero;
+        }
+      }
     }
 }
