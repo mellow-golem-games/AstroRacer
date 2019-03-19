721a2a8890a87ed3eaeb58df244c0aaa1978a6eb
peterjewicz <peterjewicz@totalwebconnections.com>
2019-03-19 08:03:32 -0500

    Player spawn + bg scroll start

---
+++ Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+
+    public GameObject ship1;
+    public GameObject ship2;
+    public GameObject ship3;
+    public float ySpawnDistance = -4.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      // Spawns the ship
+      // TODO we need to create a way to get the ship choice from the home screen - for now just
+      // spawn the first one.
+      Instantiate(ship1, new Vector3(0,ySpawnDistance,0), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/WallScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScroller : MonoBehaviour
+{
+
+    public float scrollSpeed = 0.25f;
+
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+      startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      Vector3 currentPosition = transform.position;
+      currentPosition.y = currentPosition.y - scrollSpeed / 32;
+      transform.position = currentPosition;
+    }
+}
