8750c261344cf5a6811132566a9e41106296cbb9
peterjewicz <peterjewicz@totalwebconnections.com>
2019-03-20 07:41:35 -0500

    Background repeat

--- Assets/Scripts/WallScroller.cs
+++ Assets/Scripts/WallScroller.cs
@@ -21,4 +21,10 @@ public class WallScroller : MonoBehaviour
       currentPosition.y = currentPosition.y - scrollSpeed / 32;
       transform.position = currentPosition;
     }
+
+    // Moves the wall to the top of the screen to repeat
+    void OnBecameInvisible() {
+      float newYVal = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)).y * 2;
+      transform.position = new Vector3(transform.position.x, newYVal, transform.position.z);
+    }
 }
