ebd6618dd6f2bf8c739b93ebd8e36320d6ae0b8a
peterjewicz <peterjewicz@totalwebconnections.com>
2019-07-01 08:26:00 -0500

    update start/end bg

--- Assets/Scripts/WallScroller.cs
+++ Assets/Scripts/WallScroller.cs
@@ -22,7 +22,7 @@ public class WallScroller : MonoBehaviour
 
     // Moves the wall to the top of the screen to repeat
     void OnBecameInvisible() {
-      if (transform != null && Camera.main != null) {
+      if (transform != null && Camera.main != null) { // check here or it will error out on scene switch
         float newYVal = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0)).y * 2;
         transform.position = new Vector3(transform.position.x, newYVal, transform.position.z);
       }
